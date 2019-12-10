519e27e200dfbb0a9568c73a0d46bb23764d784e
Kaden Schusler <kaden.schusler@kearsarge.org>
2019-12-10 11:25:50 -0500

    Started with the window and name, then controls!

--- Game.py
+++ Game.py
@@ -7,14 +7,24 @@ from SaltSpike import *
 from Sounds import *
 from Steak import *
 from Tilesets import *
+pygame.init()
+win = pygame.display.set_mode((500, 500))
+pygame.display.set_caption("Veggie Girl")
 
-clock = pygame.time.Clock();
+x = 50
+y = 50
+height = 60
+width = 40
+vel = 5
+size = [500, 500]
 
-size = [900, 700]
-screen = pygame.display.set_mode(size)
+isJump = False
+jumpCount = 10
 
-
-screen.fill((64, 128, 255))
-pygame.display.flip()
-clock.tick(60)
-print(clock.get_fps())
+run = True
+while run:
+    pygame.time.delay(50)
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
