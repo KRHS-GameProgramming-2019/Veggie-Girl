3e8058cd7bc66e7a83cbf5e6224bbb1a8bff3d40
Ryan St. Lawrence <ryan.stlawrence@kearsarge.org>
2019-12-19 11:33:22 -0500

    added playing music

--- Sounds.py
+++ Sounds.py
@@ -1,1 +1,17 @@
 import pygame, sys, math, random
+
+pygame.init()
+
+pygame.mixer.init()
+pygame.mixer.music.load("Sounds/631160_Domyeah---Final-Boss.ogg")
+    
+pygame.mixer.music.play(loops=-1, start=0.0)
+
+size = [900, 700]
+screen = pygame.display.set_mode(size)
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit();
