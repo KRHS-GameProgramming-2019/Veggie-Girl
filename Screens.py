6c1e552096107ca4e733f63361cbb7f733ca1fcc
Kaden Schusler <kaden.schusler@kearsarge.org>
2020-01-30 10:54:50 -0500

    fixed exit problem

--- Screens.py
+++ Screens.py
@@ -2,8 +2,3 @@ def TitleScreen(debug = False):
     if debug: print("TitleScreen Function")
     
     backgroundImage = (r"C:\Users\Student\Documents\Game Programming\Veggie Girl\Images\Backgrounds\titlescreen.png")
-
-def PauseScreen(debug = False):
-    if debug: print("PauseScreen Function")
-    
-    backgroundImage = (r"C:\Users\Student\Documents\Game Programming\Veggie Girl\Images\Backgrounds\PauseScreen.png")
